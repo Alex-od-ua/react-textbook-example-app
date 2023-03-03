a2807253d9d4c904d0401831d7eff257d9d00112
Alex <ais_alex@ukr.net>
2023-03-03 23:30:40 +0200

    add actions

--- src/components/TaskForm/TaskForm.js
+++ src/components/TaskForm/TaskForm.js
@@ -1,10 +1,20 @@
+import { useDispatch } from 'react-redux';
+import { addTask } from 'redux/actions';
+
 import { Button } from 'components/Button/Button';
 import css from './TaskForm.module.css';
 
 export const TaskForm = () => {
+  const dispatch = useDispatch();
+
+  // Вызываем генератор экшена и передаем текст задачи для поля payload
+  // Отправляем результат - экшен создания задачи
   const handleSubmit = event => {
     event.preventDefault();
     const form = event.target;
+
+    dispatch(addTask(form.elements.text.value));
+
     form.reset();
   };
 
---
+++ src/redux/selectors.js
@@ -0,0 +1,3 @@
+export const getTasks = state => state.tasks;
+
+export const getTasksstatusFilter = state => state.filters.status;
