0c7874ae53a6a17adc25a51be844168b34cbb309
Alex <ais_alex@ukr.net>
2023-03-09 13:47:49 +0200

    Summary

---
+++ src/components/TaskForm/TaskForm.js
@@ -0,0 +1,32 @@
+import { useDispatch } from 'react-redux';
+import { addTask } from 'redux/tasksSlice';
+
+import { Button } from 'components/Button/Button';
+import css from './TaskForm.module.css';
+
+export const TaskForm = () => {
+  const dispatch = useDispatch();
+
+  // Вызываем генератор экшена и передаем текст задачи для поля payload
+  // Отправляем результат - экшен создания задачи
+  const handleSubmit = event => {
+    event.preventDefault();
+    const form = event.target;
+
+    dispatch(addTask(form.elements.text.value));
+
+    form.reset();
+  };
+
+  return (
+    <form className={css.form} onSubmit={handleSubmit}>
+      <input
+        className={css.field}
+        type="text"
+        name="text"
+        placeholder="Enter task text..."
+      />
+      <Button type="submit">Add task</Button>
+    </form>
+  );
+};
--- src/redux/tasksSlice.js
+++ src/redux/tasksSlice.js
@@ -1,34 +1,46 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+
+const tasksInitialState = [
+  { id: 0, text: 'Learn HTML and CSS', completed: true },
+  { id: 1, text: 'Get good at JavaScript', completed: true },
+  { id: 2, text: 'Master React', completed: false },
+  { id: 3, text: 'Discover Redux', completed: false },
+  { id: 4, text: 'Build amazing apps', completed: false },
+];
 
 const tasksSlice = createSlice({
   name: 'tasks',
-  initialState: {
-    items: [],
-    isLoading: false,
-    errore: null,
-  },
+  initialState: tasksInitialState,
   reducers: {
-    // Выполнится в момент старта HTTP-запроса
-    fetchingInProgress(state) {
-      state.isLoading = true;
+    addTask: {
+      reducer(state, action) {
+        state.push(action.payload);
+      },
+      prepare(text) {
+        return {
+          payload: {
+            text,
+            id: nanoid(),
+            completed: false,
+          },
+        };
+      },
     },
-
-    // Выполнится если HTTP-запрос завершился успешно
-    fetchingSuccess(state, action) {
-      state.isLoading = false;
-      state.errore = null;
-      state.items = action.payload;
+    deleteTask(state, action) {
+      const index = state.findIndex(task => task.id === action.payload);
+      state.splice(index, 1);
     },
-
-    // Выполнится если HTTP-запрос завершился с ошибкой
-    fetchingError(state, action) {
-      state.isLoading = false;
-      state.error = action.payload;
+    toggleCompleted(state, action) {
+      for (const task of state) {
+        if (task.id === action.payload) {
+          task.completed = !task.completed;
+          break;
+        }
+      }
     },
   },
 });
 
-export const { fetchingInProgress, fetchingSuccess, fetchingError } =
-  tasksSlice.actions;
-
+// Экспортируем генераторы экшенов и редюсер
+export const { addTask, deleteTask, toggleCompleted } = tasksSlice.actions;
 export const tasksReducer = tasksSlice.reducer;
